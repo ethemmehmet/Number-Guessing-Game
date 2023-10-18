1b4133093e7658d70c26dbdd3119529e1825b30f
Ethem Mehmet MAVİŞ <1030521076@erciyes.edu.tr>
2023-10-18 16:17:28 +0300

    Initial commit

---
+++ Sayı Tahmin Oyunu/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Sayı_Tahmin_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bilgiLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.bilgiLabel);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 99);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(326, 159);
+            this.panel1.TabIndex = 0;
+            this.panel1.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(199, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Remaining Chances: 3";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(128, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "New Game";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Guess";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bilgiLabel
+            // 
+            this.bilgiLabel.AutoSize = true;
+            this.bilgiLabel.Location = new System.Drawing.Point(17, 102);
+            this.bilgiLabel.Name = "bilgiLabel";
+            this.bilgiLabel.Size = new System.Drawing.Size(0, 13);
+            this.bilgiLabel.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "My Guess:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(7, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(319, 52);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "I have chosen a number between 1 and 10, can you guess it?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(39, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NUMBER GUESSİNG GAME";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(148, 57);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 266);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label bilgiLabel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Sayı Tahmin Oyunu/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayı_Tahmin_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int chances = 3, enteredNumber, randomNumber;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Size = new Size(366, 305);
+            randomNumber = rnd.Next(1, 10);
+            button3.Visible = false;
+            panel1.Visible = true;
+            panel1.Location = new Point(12, 59);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chances = 3;
+            bilgiLabel.Text = "";
+            bilgiLabel.ForeColor = Color.Black;
+            label4.Text = "Remaining chances: " + (chances);
+            textBox1.Clear();
+            textBox1.Enabled = true;
+            button1.Enabled = true;
+            button2.Visible = false;
+            randomNumber = rnd.Next(1,10);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Size = new Size(366, 131);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            enteredNumber = Convert.ToInt32(textBox1.Text);
+            if(enteredNumber == randomNumber) 
+            {
+                bilgiLabel.Text = "Congratulations, correct guess!";
+                bilgiLabel.ForeColor = Color.Green;
+                textBox1.Enabled = false;
+                button1.Enabled = false;
+                button2.Visible = true;
+            }
+            else if(enteredNumber < randomNumber && chances > 1)
+            {
+                bilgiLabel.Text = "The number I guessed is higher!";
+            }
+            else if(enteredNumber > randomNumber && chances > 1)
+            {
+                bilgiLabel.Text = "The number I guessed is smaller!";
+            }
+            else
+            {
+                bilgiLabel.Text = "You've run out of guesses!";
+                textBox1.Enabled = false;
+                button1.Enabled = false;
+                button2.Visible = true;
+            }
+            chances--;
+            label4.Text = "Remaining chances: " + (chances);
+            
+        }
+    }
+}
